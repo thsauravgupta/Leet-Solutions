8a4ca2ee87a374fdaa512b1c36cb714ad353278e
thsauravgupta <120391082+thsauravgupta@users.noreply.github.com>
2025-07-21 20:07:47 +0545

    [53] [Time Beats: 99.52%] [Memory Beats: 81.42%] - LeetPush

---
+++ root/Maximum-Subarray.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int maxi = nums[0]; 
+        int sum = 0;
+        int n=nums.length;
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+            if (sum > maxi) {
+                maxi = sum;
+            }
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+        return maxi;
+    }
+}
