1be7004c604ac0c681f5e225939e041a6269af7b
thsauravgupta <120391082+thsauravgupta@users.noreply.github.com>
2025-07-30 11:33:39 +0530

    [61] [Time Beats: 100.00%] [Memory Beats: 5.01%] - LeetPush

---
+++ root/Rotate-List.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        if(head==null || head.next==null || k==0) return head;
+        int length = 1;
+        ListNode tail = head;
+        while (tail.next != null) {
+            tail = tail.next;
+            length++;
+        }
+        tail.next = head;
+        k = k % length;
+        int stepsToNewHead = length - k;
+        ListNode newTail = tail;
+
+        while (stepsToNewHead-- > 0) {
+            newTail = newTail.next;
+        }
+        ListNode newHead = newTail.next;
+        newTail.next = null;
+
+        return newHead;
+    }
+}
