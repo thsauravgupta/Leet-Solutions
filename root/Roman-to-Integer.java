5abf6b089969a43305a62cd6d7dc2c616d68613d
thsauravgupta <120391082+thsauravgupta@users.noreply.github.com>
2025-08-05 09:05:44 +0530

    [13] [Time Beats: 100.00%] [Memory Beats: 68.24%] - LeetPush

--- root/Roman-to-Integer.java
+++ root/Roman-to-Integer.java
@@ -1,22 +1,37 @@
 class Solution {
     public int romanToInt(String s) {
-        int res=0;
-        Map<Character,Integer> roman=new HashMap<>();
-        roman.put('I',1);
-        roman.put('V',5);
-        roman.put('X',10);
-        roman.put('L', 50);
-        roman.put('C', 100);
-        roman.put('D', 500);
-        roman.put('M', 1000);
-
-        for(int i=0;i<s.length()-1;i++){
-            if(roman.get(s.charAt(i))<roman.get(s.charAt(i+1))){
-                res-=roman.get(s.charAt(i));
-            }else{
-                res+=roman.get(s.charAt(i));
+       int sum=0,prev=0;
+        for(int i=s.length()-1;i>=0;i--){
+            char cur=s.charAt(i);
+            int curr=0;
+            if(cur=='I'){
+                curr= 1;
             }
+            if(cur=='V'){
+                curr= 5;
+            }
+            if(cur=='X'){
+                curr= 10;
+            }
+            if(cur=='L'){
+                curr= 50;
+            }
+            if(cur=='C'){
+                curr= 100;
+            }
+            if(cur=='D'){
+                curr= 500;
+            }
+            if(cur=='M'){
+                curr= 1000;
+            }
+            if(curr<prev){
+                sum-=curr;
+            }else{
+                sum+=curr;
+            }
+            prev=curr;
         }
-        return res + roman.get(s.charAt(s.length() - 1));
+        return sum;
     }
 }
